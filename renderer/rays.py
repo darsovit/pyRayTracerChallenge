1b55652ecdeb6ff9bad9537d8503b5af2ce9fd82
Jonathan Cameron <darsovit@gmail.com>
2020-01-04 18:33:32 -0700

    Add Initial Rays
    Adds the initial tests and implementation for Rays which
    consist of a point and vector and a Position calculation
    given an amount of time.

---
+++ renderer/rays.py
@@ -0,0 +1,17 @@
+#! python
+#
+#
+
+class Ray:
+    def __init__(self, origin, direction):
+        self.origin = origin
+        self.direction = direction
+
+    def Origin(self):
+        return self.origin
+
+    def Direction(self):
+        return self.direction
+
+    def Position(self, time):
+        return self.origin + ( self.direction * time )
---
+++ tests/features/steps/rays.py
@@ -0,0 +1,46 @@
+#! python
+#
+# rays.feature steps
+#
+
+from behave import given, then
+from renderer.rays import Ray
+from renderer.bolts import Point, Vector
+
+@when(u'{rayvar:w} ← ray({originvar:w}, {directionvar:w})')
+def step_impl(context, rayvar, originvar, directionvar):
+    print(u'STEP: When {} ← ray({}, {})'.format(rayvar, originvar, directionvar))
+    assert originvar in context.result
+    assert directionvar in context.result
+    context.result[rayvar] = Ray( context.result[originvar], context.result[directionvar] )
+
+@then(u'{rayvar:w}.origin = {originvar:w}')
+def step_impl(context, rayvar, originvar):
+    print(u'STEP: Then {}.origin = {}'.format(rayvar, originvar))
+    assert rayvar in context.result
+    assert originvar in context.result
+    assert context.result[rayvar].Origin() == context.result[originvar]
+
+@then(u'{rayvar:w}.direction = {directionvar:w}')
+def step_impl(context, rayvar, directionvar):
+    print(u'STEP: Then r.direction = direction'.format(rayvar, directionvar))
+    assert rayvar in context.result
+    assert directionvar in context.result
+    assert context.result[rayvar].Direction() == context.result[directionvar]
+
+@given(u'{rayvar:w} ← ray(point({pointx:g}, {pointy:g}, {pointz:g}), vector({vecx:g}, {vecy:g}, {vecz:g}))')
+def step_impl(context, rayvar, pointx, pointy, pointz, vecx, vecy, vecz):
+    print(u'STEP: Given r ← ray(point(2, 3, 4), vector(1, 0, 0))'.format(rayvar, pointx, pointy, pointz, vecx, vecy, vecz))
+    if 'result' not in context:
+        context.result = {}
+    context.result[rayvar] = Ray(Point(pointx, pointy, pointz), Vector(vecx, vecy, vecz))
+    #raise NotImplementedError(u'STEP: Given r ← ray(point(2, 3, 4), vector(1, 0, 0))')
+
+@then(u'position({rayvar:w}, {time:g}) = point({pointx:g}, {pointy:g}, {pointz:g})')
+def step_impl(context, rayvar, time, pointx, pointy, pointz):
+    print(u'STEP: Then position({}, {}) = point({}, {}, {})'.format(rayvar, time, pointx, pointy, pointz))
+    assert rayvar in context.result
+    expected = Point(pointx, pointy, pointz)
+    result = context.result[rayvar].Position(time)
+    assert expected == result, 'Expected ray {} position at time {} to be {}, result was {}'.format(rayvar, time, expected, result)
+    #raise NotImplementedError(u'STEP: Then position(r, 0) = point(2, 3, 4)')
